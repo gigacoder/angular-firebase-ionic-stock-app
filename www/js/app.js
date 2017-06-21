5bfda9d1f5fc12b17e370d7753d710d75e8fb844
Parul Mishra <brunocoder@yahoo.com>
2017-06-21 11:07:20 -0700

    Update app.js

--- www/js/app.js
+++ www/js/app.js
@@ -13,45 +13,22 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services', '
 
 function ApplicationRun($ionicPlatform, $rootScope, $state) {
   $ionicPlatform.ready(function() {
-    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-    // for form inputs)
     if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
       cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
       cordova.plugins.Keyboard.disableScroll(true);
     }
     if (window.StatusBar) {
-      // org.apache.cordova.statusbar required
       StatusBar.styleDefault();
     }
   });
 
   $rootScope.$on('$stateChangeError', function(event, toState, toParams, fromState, fromParams, error) {
-    // We can catch the error thrown when the $requireAuth promise is rejected
-    // and redirect the user back to the home page
     if (error === 'AUTH_REQUIRED') {
       $state.go('login');
     }
   });
-
-
-
-// $rootScope.dashtype.child('portfolio').orderByChild('Namr').equalTo($rootScope.auth.Name).on('value', function(snapshot){
-//     snapshot.forEach(function(userSnapshot) {
-//         $rootScope.user = userSnapshot.val();
-//         console.log($rootScope.user);
-//     });
-// });
-
-
-
 }
 ApplicationRun.$inject = ['$ionicPlatform', '$rootScope', '$state'];
-
-
-
-
-
-
 
 function AuthDataResolver(Auth) {
   return Auth.$requireAuth();
@@ -72,11 +49,7 @@ function ApplicationConfig($stateProvider, $urlRouterProvider) {
     templateUrl: 'templates/portfolio.html',
     controller: 'PortfolioController'
   })
-  // .state('add', {
-  //   url: '/add',
-  //   templateUrl: 'templates/add.html',
-  //   controller: 'AddController as ctrl'
-  // })
+ 
 
   .state('edit/:id', {
     url: '/edit/:id',
@@ -122,16 +95,7 @@ function ApplicationConfig($stateProvider, $urlRouterProvider) {
           }
         }
       })
-    .state('tab.chat-detail', {
-      url: '/chats/:chatId',
-      views: {
-        'tab-chats': {
-          templateUrl: 'templates/chat-detail.html',
-          controller: 'ChatDetailCtrl'
-        }
-      }
-    })
-
+ 
   .state('tab.account', {
     url: '/account',
     views: {
